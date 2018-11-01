38859ab644ea82efcc0e3375727ffe02fc265996
simonhuster <44460946+simonhuster@users.noreply.github.com>
2018-11-01 16:07:19 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,11 @@
 public class HelloWorld {
     	public HelloWorld() {
+            private int i;
+            public HelloWorld() {
+                i = 0;
+            }
+            public int getI() {
+                return i;
+            }
         }
 }
